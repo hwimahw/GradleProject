716dd44809cdbcc817857a86399a463d5a373637
Mansur <hwimahw@gmail.com>
2019-07-02 08:54:59 +0300

    Modify GradleProject

--- src/test/java/ru/ncd/MainTest.java
+++ src/test/java/ru/ncd/MainTest.java
@@ -4,6 +4,7 @@ import ru.ncd.Graph;
 import ru.ncd.Matrix;
 import ru.ncd.Island;
 import org.junit.*;
+import static org.junit.Assert.*;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
@@ -17,6 +18,21 @@ public class MainTest{
     public void buildAndCheckBuildingOfElements() throws FileNotFoundException {
         System.out.println("Матрица из файла и её размер");
         matrix = new Matrix();
+
+        assertEquals("Quantity of rows is read from file with errors", 2, matrix.n);
+        assertEquals("Quantity of columns is read from file with errors", 2, matrix.m);
+        char[] arr1 = {'#', '.', '.', '#'};
+        int size = matrix.n * matrix.m;
+        char[] arr2 = new char[size];
+        int k = 0;
+        for(int i = 0; i < matrix.n; i++) {
+            for(int j = 0; j < matrix.m; j++) {
+                arr2[k] = matrix.arrayMatrix[i][j];
+                k++;
+            }
+        }
+        assertArrayEquals("Matrix is read from file with errors", arr1, arr2);
+
         System.out.println(matrix.n + " " + matrix.m);
         for(int i = 0; i < matrix.n; i++){
             for(int j = 0; j < matrix.m; j++){
@@ -25,10 +41,35 @@ public class MainTest{
             System.out.println();
         }
 
+
+       /* assertEquals("Matrix is read from file with errors", ".....##...." +
+                "..#.....#.." +
+                ".###....#.." +
+                "......#.#.#" +
+                "...##...#.#" +
+                "##........." +
+                "##.......##" +
+                "##.......##", matrix.arrayMatrix);*/
+
         System.out.println();
 
         System.out.println("Матрица смежности графа и её размер");
         graph = new Graph(matrix);
+        assertEquals("Size of arrayGraph is not right", graph.n, matrix.n * matrix.m);
+
+        size = graph.n * graph.n;
+        char[] arr3 = new char[size];
+        char[] arr4 = {'1', '0', '0', '0', '0', '0','0', '0','0', '0','0', '0','0', '0','0','1'};
+        k = 0;
+        for(int i = 0; i < graph.n; i++){
+            for(int j = 0; j < graph.n; j++){
+                arr3[k] = graph.arrayGraph[i][j];
+                k++;
+            }
+        }
+        assertArrayEquals("graphMatrix is build with errors", arr3, arr4);
+
+
         System.out.println(graph.n);
         for(int i = 0; i < graph.n; i++){
             for(int j = 0; j < graph.n; j++){
@@ -36,11 +77,14 @@ public class MainTest{
             }
             System.out.println();
         }
+
+
         System.out.println();
     }
 
     @Test
     public void quantityOfDots(){
+
         System.out.println("Количество точек в файле");
         quantityOfDots = 0;
         for(int i = 0; i < matrix.n; i++){
@@ -50,6 +94,7 @@ public class MainTest{
                 }
             }
         }
+        assertEquals("Quantity of dots is not right", quantityOfDots, 2);
         System.out.println(quantityOfDots);
         System.out.println();
     }
@@ -58,7 +103,9 @@ public class MainTest{
     public void checkResult(){
         System.out.println("Ответ");
         quantityOfDots();
-        System.out.println("Quantity of islands: " + (graph.quantityOfIslands(graph) - quantityOfDots));
+        int result = graph.quantityOfIslands(graph) - quantityOfDots;
+        assertEquals("Quantity of islands is not right", quantityOfDots, 2);
+        System.out.println("Quantity of islands: " + result);
         System.out.println();
     }
 }
